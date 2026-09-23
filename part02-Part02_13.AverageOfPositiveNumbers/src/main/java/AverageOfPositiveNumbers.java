e3e0203ab19776341e598fcbaa90127eb87f77b7
LucasElly <lucasalvesferraz@gmail.com>
2026-09-23 17:56:15 -0300

    Part 2: Ex 13 - Average Of Positive Numbers

---
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int values = 0;
+        int sum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number < 0) {
+                continue;
+            }
+            
+            values = values + 1;
+            sum = sum + number; 
+        }
+        
+        if (values == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double average = 1.0 * sum / values;
+            System.out.println(average);
+        }
+    }
+}
