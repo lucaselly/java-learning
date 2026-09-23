95fb68e6d5495fce5d341962efad5289f3faf476
LucasElly <lucasalvesferraz@gmail.com>
2026-09-23 17:56:08 -0300

    Part 2: Ex 12 - Average Of Numbers

---
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int values = 0;
+        int sum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            values = values + 1;
+            sum = sum + number; 
+        }
+        
+        double average = 1.0 * sum / values;
+        System.out.println("Average of the numbers: " + average);
+        
+    }
+}
