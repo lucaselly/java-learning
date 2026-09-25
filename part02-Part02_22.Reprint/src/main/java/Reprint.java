91b37d2d9a62ee7a63b893ee9ba17779de8a1db8
LucasElly <lucasalvesferraz@gmail.com>
2026-09-25 20:52:57 -0300

    Part 2: Ex 22 - Reprint

---
+++ part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Reprint {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);        
+        System.out.println("How many times?");
+        int manyTimes = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 0; i < manyTimes; i++) {
+            printText();
+        }        
+    }
+    
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
